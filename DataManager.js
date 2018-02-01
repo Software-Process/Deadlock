74ac604987c2062aadce52d4cf123d073f27a9d8
Vineet <koppalkarvineet@gmail.com>
2018-01-31 19:38:43 -0500

    Implemented database to keep track of all the data on the website using MongoDB and Node.js

--- DataManager.js
+++ DataManager.js
@@ -1,29 +1,12 @@
-var postsArray = [];
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/soen341db";
 
-function init() {
-	// Read from JSON and populate list
-	if (localStorage.postsRecord) {
-		postsArray = JSON.parse(localStorage.postsRecord);
-
-		for(var i = 0; i < postsArray.length; ++i) {
-			var title = postsArray[i].title;
-			addToList(title);
-		}
-	}
-}
-
-function addToJSON() {
-	var text = document.getElementById("TextBox").value;
-	var postObj = {title:text};
-	postsArray.push(postObj);
-
-	localStorage.postsRecord = JSON.stringify(postsArray);
-	addToList(text);
-}
-
-function addToList(text) {
-	var node = document.createElement("LI");
-	var textnode = document.createTextNode(text);
-	node.appendChild(textnode);
-	document.getElementById("List").appendChild(node);
-}
+MongoClient.connect(url, function(err, db) {
+  if (err) {
+  	console.log("Error creating database");
+  	throw err;
+  } else {
+  	console.log("Database has success fully been created!");
+  }  
+  db.close();
+});
