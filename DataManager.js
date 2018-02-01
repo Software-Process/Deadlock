0544cff35f0a489bda35c4720ab8553c0379c742
Vineet <koppalkarvineet@gmail.com>
2018-01-31 20:48:15 -0500

    Implemented a collection in MongoDB database for data regarding posts

--- DataManager.js
+++ DataManager.js
@@ -1,12 +1,24 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/soen341db";
 
-MongoClient.connect(url, function(err, db) {
-  if (err) {
-  	console.log("Error creating database");
-  	throw err;
-  } else {
-  	console.log("Database has successfully been created!");
-  }  
-  db.close();
+MongoClient.connect(url, function(error, db) {
+	if (error) {
+		console.log("Error creating database");
+		throw error;
+	} else {
+		console.log("Database has successfully been created!");
+	}  
+
+	var dbo = db.db("database");
+
+	// Creating a collection to store data about posts
+	dbo.createCollection("posts", function (error, response) {
+		if (error) {
+			console.log("Unable to create collection \"Posts\"");
+			throw error;
+		} else {
+			console.log("Collection \"Posts\" has successfully been created!");			
+		}
+		db.close();
+	});	
 });
