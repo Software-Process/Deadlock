ac7604d7709709ed8948520c727305e8cef2f5a0
Michael-Gagnon <m.gagnon04@hotmail.com>
2018-02-10 18:09:59 -0500

    Issue/Task #61: Addressed the bug with incrementing/decrementing vote count. Also, refactored code so that style does not appear in js files.

--- questionFrontEnd.js
+++ questionFrontEnd.js
@@ -1,39 +1,87 @@
 function changeGreen() {
-    document.getElementById("up").style.color = "green";
-    document.getElementById("down").style.color = "#03183a";
-    document.getElementById("up").onclick = function(){} ;
-    document.getElementById("down").onclick = function(){changeRed()} ;
+    var upArrow = document.getElementById("up");
+    var downArrow = document.getElementById("down");
     var vote = parseInt(document.getElementById("question-vote").innerHTML);
-    vote++;
-    document.getElementById("question-vote").innerHTML = vote;
+    if(downArrow.classList.contains("down-voted"))
+    {
+        vote+=2;
+        document.getElementById("question-vote").innerHTML = vote;
+    }
+    else
+    {
+        vote++;
+        document.getElementById("question-vote").innerHTML = vote;
+    }
+    upArrow.classList.remove("not-voted");
+    upArrow.classList.add("up-voted");
+    downArrow.classList.remove("down-voted");
+    downArrow.classList.add("not-voted");
+    upArrow.onclick = function(){} ;
+    downArrow.onclick = function(){changeRed()} ;
 }
 
 function changeRed() {
-    document.getElementById("down").style.color = "red";
-    document.getElementById("up").style.color = "#03183a";
-    document.getElementById("up").onclick = function(){changeGreen()} ;
-    document.getElementById("down").onclick = function(){} ;
+    var upArrow = document.getElementById("up");
+    var downArrow = document.getElementById("down");
     var vote = parseInt(document.getElementById("question-vote").innerHTML);
-    vote--;
-    document.getElementById("question-vote").innerHTML = vote;
+    if(upArrow.classList.contains("up-voted"))
+    {
+        vote-=2;
+        document.getElementById("question-vote").innerHTML = vote;
+    }
+    else
+    {
+        vote--;
+        document.getElementById("question-vote").innerHTML = vote;
+    }
+    upArrow.classList.remove("up-voted");
+    upArrow.classList.add("not-voted");
+    downArrow.classList.remove("not-voted");
+    downArrow.classList.add("down-voted");
+    upArrow.onclick = function(){changeGreen()} ;
+    downArrow.onclick = function(){} ;
 }
 
 function changeGreen1() {
-    document.getElementById("up1").style.color = "green";
-    document.getElementById("down1").style.color = "#03183a";
-    document.getElementById("up1").onclick = function(){} ;
-    document.getElementById("down1").onclick = function(){changeRed1()} ;
+    var upArrow = document.getElementById("up1");
+    var downArrow = document.getElementById("down1");
     var vote = parseInt(document.getElementById("reply-vote").innerHTML);
-    vote++;
-    document.getElementById("reply-vote").innerHTML = vote;
+    if(downArrow.classList.contains("down-voted"))
+    {
+        vote+=2;
+        document.getElementById("reply-vote").innerHTML = vote;
+    }
+    else
+    {
+        vote++;
+        document.getElementById("reply-vote").innerHTML = vote;
+    }
+    upArrow.classList.remove("not-voted");
+    upArrow.classList.add("up-voted");
+    downArrow.classList.remove("down-voted");
+    downArrow.classList.add("not-voted");
+    upArrow.onclick = function(){} ;
+    downArrow.onclick = function(){changeRed1()} ;
 }
 
 function changeRed1() {
-    document.getElementById("down1").style.color = "red";
-    document.getElementById("up1").style.color = "#03183a";
-    document.getElementById("up1").onclick = function(){changeGreen1()} ;
-    document.getElementById("down1").onclick = function(){} ;
+    var upArrow = document.getElementById("up1");
+    var downArrow = document.getElementById("down1");
     var vote = parseInt(document.getElementById("reply-vote").innerHTML);
-    vote--;
-    document.getElementById("reply-vote").innerHTML = vote;
+    if(upArrow.classList.contains("up-voted"))
+    {
+        vote-=2;
+        document.getElementById("reply-vote").innerHTML = vote;
+    }
+    else
+    {
+        vote--;
+        document.getElementById("reply-vote").innerHTML = vote;
+    }
+    upArrow.classList.remove("up-voted");
+    upArrow.classList.add("not-voted");
+    downArrow.classList.remove("not-voted");
+    downArrow.classList.add("down-voted");
+    upArrow.onclick = function(){changeGreen1()} ;
+    downArrow.onclick = function(){} ;
 }
