53cc0db38d1b1cc106ed3d4be95428b98108adb5
Michael-Gagnon <m.gagnon04@hotmail.com>
2018-02-10 20:59:31 -0500

    Task #59 & 58: This commit implements the feature for accepting and rejecting replies #59 and also a quick feature fix for reseting votes #58

--- questionFrontEnd.js
+++ questionFrontEnd.js
@@ -1,4 +1,5 @@
 function changeGreen() {
+    //BACKEND: Go into database and increment vote value by 1 or 2 (or decrement by 1 for reset) depending on if the user has already downvoted.
     var upArrow = document.getElementById("up");
     var downArrow = document.getElementById("down");
     var vote = parseInt(document.getElementById("question-vote").innerHTML);
@@ -6,82 +7,160 @@ function changeGreen() {
     {
         vote+=2;
         document.getElementById("question-vote").innerHTML = vote;
+        upArrow.classList.remove("not-voted");
+        upArrow.classList.add("up-voted");
+        downArrow.classList.remove("down-voted");
+        downArrow.classList.add("not-voted");
+    }
+    else if (upArrow.classList.contains("up-voted"))
+    {
+        vote--;
+        document.getElementById("question-vote").innerHTML = vote;
+        upArrow.classList.add("not-voted")
+        upArrow.classList.remove("up-voted");
     }
     else
     {
         vote++;
         document.getElementById("question-vote").innerHTML = vote;
+        upArrow.classList.remove("not-voted");
+        upArrow.classList.add("up-voted");
+        downArrow.classList.remove("down-voted");
+        downArrow.classList.add("not-voted");
     }
-    upArrow.classList.remove("not-voted");
-    upArrow.classList.add("up-voted");
-    downArrow.classList.remove("down-voted");
-    downArrow.classList.add("not-voted");
-    upArrow.onclick = function(){} ;
-    downArrow.onclick = function(){changeRed()} ;
 }
 
 function changeRed() {
+    //BACKEND: Go into database and decrement vote value by 1 or 2 (or increment by 1 for reset) depending on if the user has already upvoted.
     var upArrow = document.getElementById("up");
     var downArrow = document.getElementById("down");
     var vote = parseInt(document.getElementById("question-vote").innerHTML);
-    if(upArrow.classList.contains("up-voted"))
+    if (downArrow.classList.contains("down-voted"))
+    {
+        vote++;
+        document.getElementById("question-vote").innerHTML = vote;
+        downArrow.classList.add("not-voted");
+        downArrow.classList.remove("down-voted");
+    }
+    else if(upArrow.classList.contains("up-voted"))
     {
         vote-=2;
         document.getElementById("question-vote").innerHTML = vote;
+        upArrow.classList.remove("up-voted");
+        upArrow.classList.add("not-voted");
+        downArrow.classList.remove("not-voted");
+        downArrow.classList.add("down-voted");
     }
     else
     {
         vote--;
         document.getElementById("question-vote").innerHTML = vote;
+        upArrow.classList.remove("up-voted");
+        upArrow.classList.add("not-voted");
+        downArrow.classList.remove("not-voted");
+        downArrow.classList.add("down-voted");
     }
-    upArrow.classList.remove("up-voted");
-    upArrow.classList.add("not-voted");
-    downArrow.classList.remove("not-voted");
-    downArrow.classList.add("down-voted");
-    upArrow.onclick = function(){changeGreen()} ;
-    downArrow.onclick = function(){} ;
 }
 
-function changeGreen1() {
-    var upArrow = document.getElementById("up1");
-    var downArrow = document.getElementById("down1");
-    var vote = parseInt(document.getElementById("reply-vote").innerHTML);
+function changeGreen1(replyContent) {
+    //BACKEND: Go into database and increment vote value by 1 or 2 (or decrement by 1 for reset) depending on if the user has already downvoted.
+    var upArrow = replyContent.childNodes[1];
+    var downArrow = replyContent.childNodes[5];
+    var voteObject = replyContent.childNodes[3];
+    var vote = parseInt(voteObject.innerHTML);
     if(downArrow.classList.contains("down-voted"))
     {
         vote+=2;
-        document.getElementById("reply-vote").innerHTML = vote;
+        voteObject.innerHTML = vote;
+        upArrow.classList.remove("not-voted");
+        upArrow.classList.add("up-voted");
+        downArrow.classList.remove("down-voted");
+        downArrow.classList.add("not-voted");
+    }
+    else if (upArrow.classList.contains("up-voted"))
+    {
+        vote--;
+        voteObject.innerHTML = vote;
+        upArrow.classList.add("not-voted")
+        upArrow.classList.remove("up-voted");
     }
     else
     {
         vote++;
-        document.getElementById("reply-vote").innerHTML = vote;
+        voteObject.innerHTML = vote;
+        upArrow.classList.remove("not-voted");
+        upArrow.classList.add("up-voted");
+        downArrow.classList.remove("down-voted");
+        downArrow.classList.add("not-voted");
     }
-    upArrow.classList.remove("not-voted");
-    upArrow.classList.add("up-voted");
-    downArrow.classList.remove("down-voted");
-    downArrow.classList.add("not-voted");
-    upArrow.onclick = function(){} ;
-    downArrow.onclick = function(){changeRed1()} ;
 }
 
-function changeRed1() {
-    var upArrow = document.getElementById("up1");
-    var downArrow = document.getElementById("down1");
-    var vote = parseInt(document.getElementById("reply-vote").innerHTML);
-    if(upArrow.classList.contains("up-voted"))
+function changeRed1(replyContent) {
+    //BACKEND: Go into database and decrement vote value by 1 or 2 (or increment by 1 for reset) depending on if the user has already upvoted.
+    var upArrow = replyContent.childNodes[1];
+    var downArrow = replyContent.childNodes[5];
+    var voteObject = replyContent.childNodes[3];
+    var vote = parseInt(voteObject.innerHTML);
+    if (downArrow.classList.contains("down-voted"))
+    {
+        vote++;
+        voteObject.innerHTML = vote;
+        downArrow.classList.add("not-voted");
+        downArrow.classList.remove("down-voted");
+    }
+    else if(upArrow.classList.contains("up-voted"))
     {
         vote-=2;
-        document.getElementById("reply-vote").innerHTML = vote;
+        voteObject.innerHTML = vote;
+        upArrow.classList.remove("up-voted");
+        upArrow.classList.add("not-voted");
+        downArrow.classList.remove("not-voted");
+        downArrow.classList.add("down-voted");
     }
     else
     {
         vote--;
-        document.getElementById("reply-vote").innerHTML = vote;
+        voteObject.innerHTML = vote;
+        upArrow.classList.remove("up-voted");
+        upArrow.classList.add("not-voted");
+        downArrow.classList.remove("not-voted");
+        downArrow.classList.add("down-voted");
     }
-    upArrow.classList.remove("up-voted");
-    upArrow.classList.add("not-voted");
-    downArrow.classList.remove("not-voted");
-    downArrow.classList.add("down-voted");
-    upArrow.onclick = function(){changeGreen1()} ;
-    downArrow.onclick = function(){} ;
+}
+
+function accept(reply) {
+    if (confirm("Are you sure you want to accept this reply? Deleting an accepted reply will only be implemented in a future sprint!"))
+    {
+        var acceptedSpace = document.getElementById("accepted-answer");
+        if(!acceptedSpace.classList.contains("hidden"))
+        {
+            alert("Sorry! Only one reply can be accepted per question.");
+        }
+        else
+        {
+            document.getElementById("accept-title").classList.remove("hidden");
+            document.getElementById("temp-accept").classList.add("hidden");
+            // BACKEND: Manipulate database so that this question.reply has ACCEPTED = True in database;
+            acceptedSpace.innerHTML = reply.childNodes[1].innerHTML;
+            acceptedSpace.classList.toggle("hidden");
+            reply.style.display = "none";
+        }
+    }
+}
+
+function reject(reply) {
+    // BACKEND: Go into database and delete this question.reply
+    reply.classList.add("hidden");
+}
+
+function frontEndSubmit() {
+    /*
+     KEEP IN MIND: This is a temporary function to test front-end features, this will not exist in the final product.
+    */
+    var replyList = document.getElementById("replies-list");
+    //The structure of a question in string format.
+    replyList.innerHTML += "<div class=\"reply\">\r\n                  <div class=\"content\">\r\n                        <i class=\"glyphicon glyphicon-chevron-up not-voted\" id=\"up1\" onclick=\"changeGreen1(this.parentElement)\"></i>\r\n                        <span class=\"reply-vote\" id=\"reply-vote\">0</span>\r\n                        <i class=\"glyphicon glyphicon-chevron-down not-voted\" id=\"down1\" onclick=\"changeRed1(this.parentElement)\"></i>\r\n                        question.reply.CONTENT\r\n                    </div>\r\n                    <button type=\"button\" class=\"btn btn-dark accept-reject\" onclick=\"accept(this.parentElement)\">Accept</button>\r\n                    <button type=\"button\" class=\"btn btn-dark accept-reject\" onclick=\"reject(this.parentElement)\">Reject</button>\r\n                    <p class=\"small pull-right\">Submitted by question.reply.USER on question.reply.DATE</p>\r\n                </div>"
+    
+
+
 }
