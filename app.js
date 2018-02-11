a4b8ece5f64b47797fb136a30f4a10dbde225a01
Kevin Janeiro <kevinjaneiro100.02@gmail.com>
2018-02-11 14:27:48 -0500

    [#71] Previous commit unsuccessful in removing handlebars, handlebars and index.js removed

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
