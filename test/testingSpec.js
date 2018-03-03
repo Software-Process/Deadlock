137af76eb43e34a6239807f14ec7a47915ac1afd
Dmi <@>
2018-03-02 22:10:46 -0500

    Fixed typo. [#90]

--- test/testingSpec.js
+++ test/testingSpec.js
@@ -1,6 +1,6 @@
 var chai = require('chai');
 var chaiHttp = require('chai-http');
-var server = require('../server/app');
+var server = require('../app');
 var should = chai.should();
 
 chai.use(chaiHttp);
