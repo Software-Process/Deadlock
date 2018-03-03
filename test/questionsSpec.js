87d92be0299750173bb19c85b238e3cae1d2cb43
Mstc08 <31962671+Mstc08@users.noreply.github.com>
2018-03-03 18:11:36 -0500

    Modified the mongodb url to connect to database [#90]
    Corrected the mongodb link to connect to our database [#90]

--- test/questionsSpec.js
+++ test/questionsSpec.js
@@ -24,7 +24,7 @@ const testSchema = mongoose.Schema({
 const MochaTest = mongoose.model('MochaTest', testSchema);
 describe('Database Tests', function() {
   before(function (done) {
-    mongoose.connect('mongodb://localhost/testDatabase');
+    mongoose.connect('mongodb://soen341:soen341@soen341-shard-00-00-ruxjj.mongodb.net:27017,soen341-shard-00-01-ruxjj.mongodb.net:27017,soen341-shard-00-02-ruxjj.mongodb.net:27017/test?ssl=true&replicaSet=SOEN341-shard-0&authSource=admin');
     const db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error'));
     db.once('open', function() {
@@ -108,7 +108,7 @@ describe('Database Tests', function() {
       });
     });
   });
-  
+
   after(function(done){
     mongoose.connection.db.dropDatabase(function(){
       mongoose.connection.close(done);
