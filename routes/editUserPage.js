18281a71c5a655402a0192c4db7d29d54ea2b320
Dmi <@>
2018-03-04 21:12:48 -0500

    Single modifiable entry, modified request to post and added a reload. [#97]

--- routes/editUserPage.js
+++ routes/editUserPage.js
@@ -9,15 +9,17 @@ router.get('/', function(req, res, next) {
 
 router.patch('/', function(req, res, next) {
     const name = req.user.username;
-    User.update({username : name},{$set: {bio : req.query.bio}})
+    User.update({username : name},{$set: {bio : req.body.bio}})
         .exec()
         .then(function(result){
-            res.render('userPage', {user : req.user });
+            res.render('userPage', {user : req.user }, function(err, resp){
+                res.redirect('userPage')
+            })
         })
         .catch(function(err){
             console.log(err);
             res.status(500).json({error:err});
-        });
+        })
 });
 
 module.exports = router;
