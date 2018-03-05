226f9f620656b563d6b2bf654eaf41d0d1530c97
Panorea <panorea.kontis@gmail.com>
2018-03-04 19:29:53 -0500

    [#99] UserPage displays questions asked by logged in user

--- routes/userPage.js
+++ routes/userPage.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+const Question = require("../models/question");
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('userpage');
+  	Question.find({user:'testUser'}) // To be replaced with logged in user
+	  	.exec()
+	  	.then(docs => {
+			console.log(docs);
+			res.render('userPage', { questions: docs });		
+	  	})
+	  	.catch(err => {
+	  		console.log(err);
+	  		res.status(200).json({
+	  			error: err
+	  	});
+  	});	
 });
 
 module.exports = router;
