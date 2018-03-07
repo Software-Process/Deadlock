cb4c22090b5d6fda36179ce9d104e0ded5fb4ef6
Panorea <panorea.kontis@gmail.com>
2018-03-06 21:44:45 -0500

    [#99] User Page displays the logged in user's questions and answers

--- routes/userPage.js
+++ routes/userPage.js
@@ -6,8 +6,33 @@ const Question = require("../models/question");
 /* GET home page. */
 router.get('/', function(req, res, next) {
     if (req.user){
-        const genderBoolean = req.user.gender == 'true';
-        res.render('userpage', {user : req.user , gender : genderBoolean});
+        var answerDocs;
+        var questionDocs;
+        Question.find({user:req.user.username})
+            .exec()
+            .then(docs1 => {
+            console.log(docs1);
+            questionDocs = docs1;
+        Question.find({'replies.user' : req.user.username})
+            .exec()
+            .then(docs2 => {
+            console.log(docs2);
+        answerDocs = docs2;
+        res.render('userpage', {user : req.user , answers : answerDocs, questions: questionDocs});
+    })
+    .catch(err => {
+            console.log(err);
+        res.status(200).json({
+            error: err
+        });
+    });
+    })
+    .catch(err => {
+            console.log(err);
+        res.status(200).json({
+            error: err
+        });
+    });
 
     } else {
         res.render('signIn');
