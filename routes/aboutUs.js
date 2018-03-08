db96258970317cb137e34c5a7d708a493e126ab4
Kevin Janeiro <kevinjaneiro100.02@gmail.com>
2018-03-07 22:58:49 -0500

    Converted tab indentation to 4-space indentation

--- routes/aboutUs.js
+++ routes/aboutUs.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET About Us page. */
 router.get('/', function(req, res, next) {
-	res.render('aboutus');
+    res.render('aboutus');
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -5,22 +5,22 @@ const question = require("../models/question");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  	question.find()
-	  	.exec()
-	  	.then(docs => {
-			if (req.user){
+    question.find()
+        .exec()
+        .then(docs => {
+            if (req.user){
                 res.render('index', { questions: docs, username : req.user.username });
             } else {
                 res.render('index', { questions: docs});
                 }
 
-	  	})
-	  	.catch(err => {
-	  		console.log(err);
-	  		res.status(200).json({
-	  			error: err
-	  	});
-  	});	
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(200).json({
+                error: err
+        });
+    }); 
 });
 
 module.exports = router;
