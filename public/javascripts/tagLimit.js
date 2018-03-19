ac0124026b526b87425bcff7af53f27e522a833b
Vineet <koppalkarvineet@gmail.com>
2018-03-18 22:49:28 -0400

    [#136] Correcting typos in question-prompt.hbs and tagLimit.js. Setting max number of tags as constant

--- public/javascripts/tagLimit.js
+++ public/javascripts/tagLimit.js
@@ -1,12 +1,12 @@
 // This ensures that only a certain number of tags (3) can be attached to a question
 $(document).ready(function () {
-  $("input[name='tag']").change(function () {
-    var maxAllowed = 3;
-    var nbBoxesChecked = $("input[name='tag']:checked").length;
-    if (nbBoxesChecked > maxAllowed) {
-      $(this).prop("checked", "");
-      $(this).parent().removeClass("active");
-      alert("Please select atmost " + maxAllowed + " tags.")
-    }
-  });
+    $("input[name='tag']").change(function () {
+        const maxAllowed = 3;
+        var nbBoxesChecked = $("input[name='tag']:checked").length;
+        if (nbBoxesChecked > maxAllowed) {
+            $(this).prop("checked", "");
+            $(this).parent().removeClass("active");
+            alert("Please select at most " + maxAllowed + " tags.")
+        }
+    });
 });
