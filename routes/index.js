b4ea9ddec474de99a478cac6e4915a708693a118
Michael-Gagnon <m.gagnon04@hotmail.com>
2018-03-21 20:53:54 -0400

    [#131] Implemented feature to sort the index page to only show question with certain tags and front end tag work

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-
 const question = require("../models/question");
 
 /* GET home page. */
@@ -14,6 +13,24 @@ router.get('/', function(req, res, next) {
                 res.render('index', { questions: docs.reverse()});
             }
 
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(200).json({
+                error: err
+        });
+    }); 
+});
+
+router.get('/:tag', function(req, res) {
+    const id = req.params.tag;
+    question.find({tags : id})
+        .then(docs => {
+            if (req.user){
+                res.render('index', { questions: docs.reverse(), user : req.user });
+            } else {
+                res.render('index', { questions: docs.reverse()});
+            }
         })
         .catch(err => {
             console.log(err);
