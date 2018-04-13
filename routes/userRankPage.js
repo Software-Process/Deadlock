16107633f5056deec4d39c5ba34fff3c47662e49
georgikbarsemian <georgik.barsemian@mail.concordia.ca>
2018-04-13 00:05:59 -0400

    [#124] Sortable by tags each user based on their score

--- routes/userRankPage.js
+++ routes/userRankPage.js
@@ -9,7 +9,7 @@ router.get('/', function(req, res, next) {
 	User.find()
         .exec()
         .then(docs => {        	
-			res.render('userRankPage', { user: req.user, users: docs, tagName : "Please select a tag above", tagScores: "-" });
+			res.render('userRankPage', { user: req.user, users: docs });
         })
         .catch(err => {
             console.log(err);
@@ -22,24 +22,218 @@ router.get('/', function(req, res, next) {
 router.get('/:tag', function(req, res) {
     const tag = req.params.tag;			//Java
     var fieldTag = getFieldWithTag(tag);	//tagJava
-        	
-    User.find()
-    	.exec()
-        .then(docs => {
-        	var tagValues = [];
-        	var query = {};
-        	for(var i = 0; i < docs.length; ++i) {    		
-        		tagValues.push(docs[i][fieldTag]);
-        	}    		
-
-			res.render('userRankPage', { user: req.user, tagName : tag, users: docs, tagField: fieldTag });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(200).json({
-                error: err
-        });
-    }); 
+   
+    switch(fieldTag){
+        case "tagJava":
+        User.find({ tagJava: { $gt: 0 } }).sort({ tagJava:-1})
+            .exec()
+            .then(docs => {
+                res.render('userRankPage', { user: req.user, tagName : tag, users: docs, tagField: fieldTag, showTable:"True" });
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(200).json({
+                    error: err
+            });
+        }); 
+        break;
+        case "tagPHP":
+        User.find({ tagPHP: { $gt: 0 } }).sort({ tagPHP:-1})
+            .exec()
+            .then(docs => {
+                res.render('userRankPage', { user: req.user, tagName : tag, users: docs, tagField: fieldTag, showTable:"True" });
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(200).json({
+                    error: err
+            });
+        }); 
+        break;
+        case "tagPython":
+        User.find({ tagPython: { $gt: 0 } }).sort({ tagPython:-1})
+            .exec()
+            .then(docs => {
+                res.render('userRankPage', { user: req.user, tagName : tag, users: docs, tagField: fieldTag, showTable:"True" });
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(200).json({
+                    error: err
+            });
+        }); 
+        break;
+        case "tagCPlusPlus":
+        User.find({ tagCPlusPlus: { $gt: 0 } }).sort({ tagCPlusPlus:-1})
+            .exec()
+            .then(docs => {
+                res.render('userRankPage', { user: req.user, tagName : tag, users: docs, tagField: fieldTag, showTable:"True" });
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(200).json({
+                    error: err
+            });
+        }); 
+        break;
+        case "tagCSharp":
+        User.find({ tagCSharp: { $gt: 0 } }).sort({ tagCSharp:-1})
+            .exec()
+            .then(docs => {
+                res.render('userRankPage', { user: req.user, tagName : tag, users: docs, tagField: fieldTag, showTable:"True" });
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(200).json({
+                    error: err
+            });
+        }); 
+        break;
+        case "tagRuby":
+        User.find({ tagRuby: { $gt: 0 } }).sort({ tagRuby:-1})
+            .exec()
+            .then(docs => {
+                res.render('userRankPage', { user: req.user, tagName : tag, users: docs, tagField: fieldTag, showTable:"True" });
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(200).json({
+                    error: err
+            });
+        }); 
+        break;
+        case "tagLisp":
+        User.find({ tagLisp: { $gt: 0 } }).sort({ tagLisp:-1})
+            .exec()
+            .then(docs => {
+                res.render('userRankPage', { user: req.user, tagName : tag, users: docs, tagField: fieldTag, showTable:"True" });
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(200).json({
+                    error: err
+            });
+        }); 
+        break;
+        case "tagProlog":
+        User.find({ tagProlog: { $gt: 0 } }).sort({ tagProlog:-1})
+            .exec()
+            .then(docs => {
+                res.render('userRankPage', { user: req.user, tagName : tag, users: docs, tagField: fieldTag, showTable:"True" });
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(200).json({
+                    error: err
+            });
+        }); 
+        break;
+        case "tagHtml":
+        User.find({ tagHtml: { $gt: 0 } }).sort({ tagHtml:-1})
+            .exec()
+            .then(docs => {
+                res.render('userRankPage', { user: req.user, tagName : tag, users: docs, tagField: fieldTag, showTable:"True" });
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(200).json({
+                    error: err
+            });
+        }); 
+        break;
+        case "tagCss":
+        User.find({ tagCss: { $gt: 0 } }).sort({ tagCss:-1})
+            .exec()
+            .then(docs => {
+                res.render('userRankPage', { user: req.user, tagName : tag, users: docs, tagField: fieldTag, showTable:"True" });
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(200).json({
+                    error: err
+            });
+        }); 
+        break;
+        case "tagJavaScript":
+        User.find({ tagJavaScript: { $gt: 0 } }).sort({ tagJavaScript:-1})
+            .exec()
+            .then(docs => {
+                res.render('userRankPage', { user: req.user, tagName : tag, users: docs, tagField: fieldTag, showTable:"True" });
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(200).json({
+                    error: err
+            });
+        }); 
+        break;
+        case "tagJade":
+        User.find({ tagJade: { $gt: 0 } }).sort({ tagJade:-1})
+            .exec()
+            .then(docs => {
+                res.render('userRankPage', { user: req.user, tagName : tag, users: docs, tagField: fieldTag, showTable:"True" });
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(200).json({
+                    error: err
+            });
+        }); 
+        break;
+        case "tagC":
+        User.find({ tagC: { $gt: 0 } }).sort({ tagC:-1})
+            .exec()
+            .then(docs => {
+                res.render('userRankPage', { user: req.user, tagName : tag, users: docs, tagField: fieldTag, showTable:"True" });
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(200).json({
+                    error: err
+            });
+        }); 
+        break;
+        case "tagFortran":
+        User.find({ tagFortran: { $gt: 0 } }).sort({ tagFortran:-1})
+            .exec()
+            .then(docs => {
+                res.render('userRankPage', { user: req.user, tagName : tag, users: docs, tagField: fieldTag, showTable:"True" });
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(200).json({
+                    error: err
+            });
+        }); 
+        break;
+        case "tagVisualBasic":
+        User.find({ tagVisualBasic: { $gt: 0 } }).sort({ tagVisualBasic:-1})
+            .exec()
+            .then(docs => {
+                res.render('userRankPage', { user: req.user, tagName : tag, users: docs, tagField: fieldTag, showTable:"True" });
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(200).json({
+                    error: err
+            });
+        }); 
+        break;
+        case "tagAssembly":
+        User.find({ tagAssembly: { $gt: 0 } }).sort({ tagAssembly:-1})
+            .exec()
+            .then(docs => {
+                res.render('userRankPage', { user: req.user, tagName : tag, users: docs, tagField: fieldTag, showTable:"True" });
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(200).json({
+                    error: err
+            });
+        }); 
+        break;
+       
+    }   
 });
 
 function getFieldWithTag(tag) {
